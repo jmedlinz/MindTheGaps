c369093e16518cd57ae5ed7582436c625c868afe
James Medlin <james@themedlins.com>
2020-01-02 11:26:44 -0600

    Added FillTheGap.ps1 so time in meetings, webinars, etc. can be counted

---
+++ constants.ps1
@@ -0,0 +1,20 @@
+# Constant that indicates how many minutes of inactivity to ignore.
+# If we're inactive for more than this many minutes then treat it as a break, and a gap begins.
+#$BreakLimit = 10;
+Set-Variable BreakLimit -option Constant -value 10
+
+# If we're not active for at least this many minutes, don't count it as "work".
+#$WorkLimit = 10;
+Set-Variable WorkLimit -option Constant -value 10
+
+# The sub-folder to process.  Defaults to the current date.
+$ThisDay = (Get-Date).AddDays(-$DaysBack).ToString("yyyy-MM-dd")
+
+# The base folder to the snapshot folders.
+#$BasePath = "J:\Snapshots"
+#Set-Variable BasePath -option Constant -value "c:\temp\gaps\$ThisDay"
+Set-Variable BasePath -option Constant -value "J:\Snapshots\$ThisDay"
+
+#The file extension that the files are saved in: png, jpg, gif, etc.
+#$FileExt = "jpg"
+Set-Variable FileExt -option Constant -value "jpg"
