5e382ab8cfb15b1660929343829f96ddd3f97a5e
James Medlin <james@themedlins.com>
2020-02-12 12:37:03 -0600

    Made the BreakLimit and WorkLimit to be more realistic and appropriate

--- constants.ps1
+++ constants.ps1
@@ -1,11 +1,11 @@
 # Constant that indicates how many minutes of inactivity to ignore.
 # If we're inactive for more than this many minutes then treat it as a break, and a gap begins.
 #$BreakLimit = 10;
-Set-Variable BreakLimit -option Constant -value 10
+Set-Variable BreakLimit -option Constant -value 15
 
 # If we're not active for at least this many minutes, don't count it as "work".
 #$WorkLimit = 10;
-Set-Variable WorkLimit -option Constant -value 10
+Set-Variable WorkLimit -option Constant -value 8
 
 # The sub-folder to process.  Defaults to the current date.
 $ThisDay = (Get-Date).AddDays(-$DaysBack).ToString("yyyy-MM-dd")
